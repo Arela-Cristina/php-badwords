ca85ccd88b38da68b46418a513397bc320085822
Sirlife <simone@freedome.it>
2024-07-23 17:18:14 +0200

    work done

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<header>
+    <h3>Security Portal</h3>
+</header>
 <body>
-    
+        <!-- inizio form, in action inserisco il nome della mia pagina di aterraggio, method post perche e un form -->
+        <form action="pagina-aterraggio.php" method="POST">
+            <!-- Form Last Name -->
+            <label for="lastName">Last Name</label>
+            <input type="text" name="lastName">
+
+            <!-- Form Code Access -->
+            <label for="accessCode">Access Code</label>
+            <input type="text" name="accessCode">
+
+             <!-- Bottone Invio -->
+            <button type="submit">Log In</button>
+        </form>
 </body>
 </html>
--- pagina-aterraggio.php
+++ pagina-aterraggio.php
@@ -1,6 +1,17 @@
 <?php
+//In questa maniera io salvo i miei valori dentro una variabile  per poi stamparli in pagina e realizare la interazione con utente.
+//salvo il valore del mio input Last Name in una variabile, e dentro le parentesi quadre scrivo il valore di 'name'del input in form.
+$inputLastName = $_POST['lastName'];
+//salvo il valore del mio input Acces Code in altra  variabile
+$inputAccessCode = $_POST['accessCode'];
 
+//Creo altra variavile per formare un solo paragrafo tra le mie prime due variabili.
+$paragraph = 'Welcome to Security Portal user acces level number'; 
+
+//Creo altra volta il paragrapho ma con il testo che io vorrei replace to ***
+$newParagraph = str_replace('number', '* * *',  $paragraph);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +21,23 @@
     <title>Document</title>
 </head>
 <body>
+    <h3>Security Portal</h3>
+    <!-- output nello schermo degli input -->
+    <h5> <?php echo $inputLastName?> <?php echo $inputAccessCode?> </h5>
     
+    <!-- output paragrafo statico y variabile $inputLastName -->
+    <p>Welcome to Security Portal <?php echo $_POST['lastName']?></p>
+    <!-- output span e variabile $inputAccessCode -->
+    <p>Access Level <?php echo $_POST['accessCode']?></p>
+
+    <!-- output variabile $paragraph-->
+    <div><?php echo $paragraph ?></div>
+    <!-- method per avere il lenght di una stringa, paragrafo, etc -->
+    <div><?php echo strlen($paragraph) ?></div>
+
+    <!-- output variabile $newParagraph -->
+    <div><?php echo $newParagraph ?></div>
+    <!-- method per avere il lenght di una stringa, paragrafo, etc -->
+    <div><?php echo strlen($newParagraph) ?></div>
 </body>
 </html>
